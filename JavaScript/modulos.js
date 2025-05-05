e6634b9ccbcb0717d55ec85c02a966407874a481
Calaza <calaza.santiago@tecnica7.edu.ar>
2025-05-05 19:09:23 -0300

    primer commit

---
+++ JavaScript/modulos.js
@@ -0,0 +1,9 @@
+document.getElementById("header").innerHTML = `<h1>inicio</h1>`;
+
+document.getElementById("nav").innerHTML = ` <ul>
+            <li><a href="index.html">inicio</a></li>
+            <li><a href="pagina1.html">galeria</a></li>
+            <li><a href="pagina2.html">contacto</a></li>
+        </ul>
+`;
+document.getElementById("footer").innerHTML = " <h3> Argentina© 2025 Southbay S.R.L. Todos los derechos reservados</h3>";
