5d19c606c9e238b86b7d77723d52f574c5f30300
Calaza <calaza.santiago@tecnica7.edu.ar>
2025-05-07 19:53:56 -0300

    septimo commit

--- JavaScript/modulos.js
+++ JavaScript/modulos.js
@@ -1,9 +1,15 @@
-document.getElementById("header").innerHTML = `<h1>inicio</h1>`;
+ const header =  `<h1>inicio</h1>`;
+ document.getElementById("header").innerHTML = header;
 
-document.getElementById("nav").innerHTML = ` <ul>
+const nav = ` <ul>
             <li><a href="index.html">inicio</a></li>
             <li><a href="pagina1.html">galeria</a></li>
             <li><a href="pagina2.html">contacto</a></li>
         </ul>
 `;
-document.getElementById("footer").innerHTML = " <h3> Argentina© 2025 Southbay S.R.L. Todos los derechos reservados</h3>";
+
+document.getElementById("nav").innerHTML = nav;
+
+
+const footer = ' <h3> Argentina© 2025 Southbay S.R.L. Todos los derechos reservados</h3>';
+document.getElementById("footer").innerHTML = footer;
